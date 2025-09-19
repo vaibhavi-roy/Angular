4ce6d3bcb78e96477245e0e3be52faa699a155c2
Vaibhavi Roy <vaibhaviroy2002@gmail.com>
2025-09-19 14:12:38 +0530

    upto computed signals; see docs also

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,4 @@
-import { Component, signal } from '@angular/core';
+import { Component, computed, effect, Signal, signal, WritableSignal, ɵunwrapWritableSignal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Login } from './login/login';
 import { Signup } from './signup/signup';
@@ -108,11 +108,81 @@ export class App {
   //   this.color = parseInt((event.target as HTMLInputElement).value);
   // }
 
-  color = 'white';
-  handleColor(val: string) {
-    this.color = val;
+  // color = 'white';
+  // handleColor(val: string) {
+  //   this.color = val;
+  // }
+  // handleInput(event: Event) {
+  //   this.color = (event.target as HTMLInputElement).value;
+  // }
+  // users = ['A', 'B', 'C', 'D', 'E'];
+  // students = [
+  //   { name: 'vaibhavi', age: 23 },
+  //   { name: 'roy', age: 24 },
+  //   { name: 'abc', age: 25 }
+  // ]
+  // getName(name: string) {
+  //   console.log(name);
+  // }
+
+  //signal example
+
+  // count = signal(10);
+  // x = 20;
+
+  // constructor() {
+  //   effect(() => {
+  //     console.log(this.x); //not called in console
+  //     console.log(this.count());
+  //   })
+  // }
+  // updateValue() {
+  //   this.x = 100;// <--- This will now trigger the effect if count iss not used
+  //   this.count.set(this.count() + 1);// <--- This will trigger the effect
+  // }
+  // updateValue1(val: string) {
+  //   if (val === 'inc') {
+  //     this.count.set(this.count() + 1);
+  //   }
+  //   else if (val === 'dec') {
+  //     this.count.set(this.count() - 1);
+  //   }
+  //   else {
+  //     this.count.set(0);
+  //   }
+  // }
+
+  // Data types in Signal
+  // data = signal(10);//number
+  // data = signal('vaibhavi');//string
+  // data = signal(true);//boolean
+  // data = signal([1, 2, 3, 4, 5]);//array
+  // data = signal({ name: 'vaibhavi', age: 23 });//object
+  // data = signal<any>(10);//any type
+  // data = signal<string | number>('vaibhavi');//more than one type
+
+  //Writable Signal
+  // data1: WritableSignal<number | string> = signal('h1');
+
+  // updateValue(val: string | number) {
+  //   // this.data1.set(val);
+  //   // this.data1.update((val) => val + 1); //not allowed
+  //   // this.data.set(val);
+  // }
+
+  //Computed Signal
+  // count: Signal<number> = computed(() => 200);// cannot use writable signal here, can't be updated
+  x = signal(10);
+  y = signal(20);
+  z = computed(() => this.x() + this.y());
+
+  showValue() {
+    console.log(this.z());
+    this.x.set(100); //update using computedsignal as dependencies are updated; cant change on their own
+    console.log(this.z());
   }
-  handleInput(event: Event) {
-    this.color = (event.target as HTMLInputElement).value;
+  updateX() {
+    this.x.set(200);
   }
+
 }
